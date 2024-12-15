c8ef050d487de1cbfc566c1ad67a383066637ade
Bartosz <96384251+hisoroo@users.noreply.github.com>
2024-12-15 20:05:42 +0100

    Add routes for LoginPage and RegisterPage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import CartPage from "./pages/CartPage/CartPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/equipment/:id" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
     </Router>
